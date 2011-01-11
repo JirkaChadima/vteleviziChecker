e0b14f17cbfd4b8a2a3e5ca00770bc7e2401176a
Jiri Chadima <chadima.jiri@gmail.com>
2011-01-11 21:35:56 +0100

    Minor update, notifications are now shown multiple times

--- src/vtelevizichecker.py
+++ src/vtelevizichecker.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 ##
-## V televizi notifier
+## vteleviziChecker
 ## -----------------------------------------------------------------------------
 ## This short python script is meant to serve as an 'integrated' notifier of
 ## TV shows in Ubuntu with libnotify. It is using the capabilites of web
@@ -21,17 +21,17 @@
 ## There are three configuration variables.
 ## - rss_base_link - link to your rss feed from vtelevizi.cz. You have to
 ##                   to include the token at the end of the URL.
+## - rss_update_time - after how many minutes the feed will be refreshed
 ## - timeshift_from_GMT - due to the feedparser time parsing method, we have to
 ##                        shift every showtime we recieve in correct timezone.
 ## - tell_m_n_minutes_before - how many minutes before the start of the show
 ##                             the notification should be displayed
 ##
 ## After configuration, the script runs in an infinite loop. Every minute, it
-## checkes whether some show is starting in n minutes. If yes, it shows the
-## notification. Every two minutes, RSS feed is checked again to include
-## updates. Items are stored in local list, every time RSS is refreshed,
-## the list is deleted. Every time a notification is shown, the show is deleted
-## from the list.
+## checkes whether some show is starting in n minutes. If yes, a notification
+## is shown. It is then shown every minute until the show starts. Every m
+## minutes, RSS feed is checked again to include updates. Items are stored
+## in local list, every time RSS is refreshed, the list is deleted.
 ##
 ##
 ## Author
@@ -53,12 +53,13 @@ import logging
 
 __author__="Jirka Chadima"
 __date__ ="$Jan 10, 2011 8:36:24 PM$"
-__version__ = "0.2"
+__version__ = "0.3"
 
 database = []
 rss_base_link = "http://vtelevizi.cz/export/rss/..."
+rss_update_time = 5
 timeshift_from_GMT = +2
-tell_me_n_minutes_before = 2
+tell_me_n_minutes_before = 5
 
 class Show:
     """Data unit for a single broadcast of a TV show"""
@@ -101,7 +102,7 @@ if __name__ == "__main__":
     counter = 0
     try:
         while True:
-            if counter % 2 == 0:
+            if counter % rss_update_time == 0:
                 refresh_rss()
                 logging.info("There are currently %i records in your personal schedule..." % len(database))
 
@@ -110,11 +111,13 @@ if __name__ == "__main__":
                     loctime = shift_loctime(datetime.datetime(*time.localtime()[:6]), tell_me_n_minutes_before)
                     showtime = shift_showtime(datetime.datetime(*database[0].timestamp[:6]), timeshift_from_GMT)
 
+                    if loctime <= showtime:
+                        del database[0]
+                        logging.info("Wiping out %s, already running" % database[0].title)
                     if loctime > showtime:
                         n = pynotify.Notification(database[0].title, showtime.strftime("%d. %m. %Y %H:%M"), "video-display")
                         n.show()
-                        logging.info("Showing and wiping out %s" % database[0].title)
-                        del database[0]
+                        logging.info("Showing %s" % database[0].title)
                 except Exception, e:
                     logging.error("V televizi exception: ", e)
             counter += 1
